73a59e944bc24ce2719e76d61edfc54b4d6e809e
SumaylaKhan <SumaylaKhan@9.199.46.253>
2018-11-14 22:33:23 +0530

    Automation project by sumayla

---
+++ E2ESeleniumJenkins/src/testCases/TC002.java
@@ -0,0 +1,27 @@
+package testCases;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class TC002 {
+WebDriver driver;
+	
+	@Test
+	public void testCase01() {
+		driver= new FirefoxDriver ();
+		System.setProperty("webdriver.gecko.driver", "C:\\selenium-java-3.13.0\\geckodriver.exe");
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com/");
+		driver.findElement(By.id("email")).sendKeys("Adnan");
+		driver.findElement(By.id("pass")).sendKeys("Ziddi");
+		
+		
+	}
+
+}
